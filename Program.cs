28bb0ab936372f44302c8ef5d5d3d22b15924895
Divitrion <santibinciguerra@gmail.com>
2022-06-07 16:50:44 -0300

    punto 1 casi terminado

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿string ruta;
+var archivos = new List<FileInfo>();
+var carpetas = new List<DirectoryInfo>();
+
+Console.WriteLine("Ingrese la ruta de un directorio: ");
+ruta=Console.ReadLine();
+
+carpetas= new DirectoryInfo(ruta).GetDirectories().ToList();
+StreamWriter index = new StreamWriter(@"C:\Users\Alumno\Desktop\Taller\Nuevacarpeta\tp08-2022-Divitrion\index.csv");
+index.WriteLine("Registro,Nombre,Extension;");
+int registro = 1;
+
+foreach (var carpeta in carpetas)
+{
+    Console.WriteLine($"Carpeta: {carpeta.Name}");
+    archivos = carpeta.GetFiles().ToList();
+    foreach (var archivo in archivos)
+    {
+        Console.WriteLine(archivo.Name);
+        index.WriteLine($"{registro},{archivo.Name},{archivo.Extension}");
+        registro++;
+    }
+}
+archivos = new DirectoryInfo(ruta).GetFiles().ToList();
+    foreach (var archivo in archivos)
+    {
+        index.WriteLine($"{registro},{archivo.Name},{archivo.Extension}");
+        Console.WriteLine(archivo.Name);
+        registro++;
+    }
+StreamReader sr = new StreamReader(@"C:\Users\Alumno\Desktop\Taller\Nuevacarpeta\tp08-2022-Divitrion\index.csv");
+index.Close();
